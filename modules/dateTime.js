8609af9932ba63a4d156705b7f021dfd873b9d9d
Enoh <eno.col@gmail.com>
2023-08-21 19:06:34 +0100

    Edit datetime.js

--- modules/dateTime.js
+++ modules/dateTime.js
@@ -1,11 +1,1 @@
-import { DateTime } from '../node_modules/luxon/build/es6/luxon.js';
 
-const date = document.querySelector('.date');
-const year = DateTime.now();
-const month = DateTime.now();
-const day = DateTime.now();
-const hour = DateTime.now();
-const minutes = DateTime.now();
-const seconds = DateTime.now();
-
-date.innerHTML = `${day.c.day}/${month.c.month}/${year.c.year} | ${hour.c.hour}:${minutes.c.minute}:${seconds.c.second}`;

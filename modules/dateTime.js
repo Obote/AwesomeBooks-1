09b3261fcca5f8a232a5a0701e82597fd890c4cf
Enoh <eno.col@gmail.com>
2023-08-21 14:44:19 +0100

    Edit .html, index.js, and styles.css

--- modules/dateTime.js
+++ modules/dateTime.js
@@ -1,1 +1,15 @@
-import { DateTime } from 'luxon';
+import { DateTime } from '../node_modules/luxon/build/es6/luxon.js';
+
+const date = document.querySelector('.date');
+console.log(date);
+const year = DateTime.now().c.year;
+const month = DateTime.now().c.month;
+const day = DateTime.now().c.day;
+const hour = DateTime.now().c.hour;
+const minutes = DateTime.now().c.minute;
+console.log(minutes);
+const seconds = DateTime.now().c.second;
+
+setTimeout(() => {
+  date.innerHTML = `${day}/${month}/${year} | ${hour}:${minutes}:${seconds}`;
+}, 100);

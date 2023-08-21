d6ad5f43225a734bfcdea5766d352379dc77b1d0
Enoh <eno.col@gmail.com>
2023-08-21 19:53:29 +0100

    Edit dateTime.js and import luxon

--- modules/dateTime.js
+++ modules/dateTime.js
@@ -1,1 +1,14 @@
+import { DateTime } from './luxon.js';
 
+const setInitialTime = () => {
+  const date = document.querySelector('.date');
+  const dt = DateTime.now();
+  const getCurrentTime = () => `${dt.hour}:${dt.minute}:${dt.second}`;
+  const f = { month: 'long', day: 'numeric' };
+
+  return (date.innerHTML = `${dt.setLocale('en-US').toLocaleString(f)} ${
+    dt.year
+  }/ <span>${getCurrentTime()}</span>`);
+};
+
+export default setInitialTime();
